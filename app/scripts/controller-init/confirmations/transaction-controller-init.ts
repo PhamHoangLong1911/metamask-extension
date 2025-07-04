1cffdfe53df928be6cc9b28bc4596521173c4630
Vinicius Stevam <vinicius.stevam@consensys.net>
2025-07-04 06:26:21 +0100

    is smart transaction disabled return undefined to be handles in the controller

--- app/scripts/controller-init/confirmations/transaction-controller-init.ts
+++ app/scripts/controller-init/confirmations/transaction-controller-init.ts
@@ -356,7 +356,7 @@ function publishBatchSmartTransactionHook({
     getSmartTransactionCommonParams(flatState, transactionMeta.chainId);
 
   if (!isSmartTransaction) {
-    return {};
+    return undefined;
   }
 
   return submitBatchSmartTransactionHook({
