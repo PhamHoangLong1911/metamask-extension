befbbccef0d3a6f4087b723b622dc3b2eef78d8e
salimtb <salim.toubal@outlook.com>
2025-07-04 13:35:34 +0200

    fix: fix linter

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -1780,8 +1780,9 @@ export default class MetamaskController extends EventEmitter {
       getMetaMetricsProps: async () => {
         const selectedAddress =
           this.accountsController.getSelectedAccount().address;
-        const accountHardwareType =
-          await this.getHardwareTypeForMetric(selectedAddress);
+        const accountHardwareType = await this.getHardwareTypeForMetric(
+          selectedAddress,
+        );
         const accountType = await this.getAccountType(selectedAddress);
         const deviceModel = await this.getDeviceModel(selectedAddress);
         return {
@@ -5584,8 +5585,9 @@ export default class MetamaskController extends EventEmitter {
    * @returns {'hardware' | 'imported' | 'snap' | 'MetaMask'}
    */
   async getAccountType(address) {
-    const keyringType =
-      await this.keyringController.getAccountKeyringType(address);
+    const keyringType = await this.keyringController.getAccountKeyringType(
+      address,
+    );
     switch (keyringType) {
       case KeyringType.trezor:
       case KeyringType.oneKey:
--- ui/components/multichain/network-list-menu/network-list-menu.tsx
+++ ui/components/multichain/network-list-menu/network-list-menu.tsx
@@ -76,7 +76,6 @@ import {
   getNetworkDiscoverButtonEnabled,
   getAllChainsToPoll,
   getEnabledNetworksByNamespace,
-  isGlobalNetworkSelectorRemoved,
 } from '../../../selectors';
 import ToggleButton from '../../ui/toggle-button';
 import {
@@ -359,13 +358,10 @@ export const NetworkListMenu = ({ onClose }: NetworkListMenuProps) => {
     if (Object.keys(tokenNetworkFilter || {}).length <= 1) {
       dispatch(setTokenNetworkFilter({ [hexChainId]: true }));
     } else {
-      const allOpts = Object.keys(evmNetworks).reduce(
-        (acc, id) => {
-          acc[id] = true;
-          return acc;
-        },
-        {} as Record<string, boolean>,
-      );
+      const allOpts = Object.keys(evmNetworks).reduce((acc, id) => {
+        acc[id] = true;
+        return acc;
+      }, {} as Record<string, boolean>);
       dispatch(setTokenNetworkFilter(allOpts));
     }
 
@@ -537,18 +533,12 @@ export const NetworkListMenu = ({ onClose }: NetworkListMenuProps) => {
             }
           : undefined,
         onRpcSelect: () => {
-          if (isGlobalNetworkSelectorRemoved) {
-            history.push('/select-rpc', {
+          setActionMode(ACTION_MODE.SELECT_RPC);
+          dispatch(
+            setEditedNetwork({
               chainId: hexChainId,
-            });
-          } else {
-            setActionMode(ACTION_MODE.SELECT_RPC);
-            dispatch(
-              setEditedNetwork({
-                chainId: hexChainId,
-              }),
-            );
-          }
+            }),
+          );
         },
       };
     },
